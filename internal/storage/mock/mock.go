24336a75368d9c9e320575253bd2a10b73b8e582
Nikolay Mazein <feynmaz@gmail.com>
2025-01-19 09:54:46 +0500

    add filters to mock storage's GetCommodities

--- internal/storage/mock/mock.go
+++ internal/storage/mock/mock.go
@@ -2,6 +2,7 @@ package mock
 
 import (
 	"context"
+	"strings"
 
 	"github.com/fungicibus/inventory/internal/types"
 )
@@ -31,10 +32,48 @@ func (m *mockStorage) GetCommodities(ctx context.Context, filters types.Commodit
 			Name:        "Fly agaric",
 			Description: "Amanita muscaria. The edible caps",
 			Price:       1,
-			Quantity:    100,
+			Quantity:    8,
 			PackageForm: "1 dried piece",
 			Sku:         "EXO-DRY-FAG",
 		},
+		{
+			Id:          "266b9823-9b87-4d73-a0f8-41a2b6c5e834",
+			Category:    "exotic",
+			Name:        "Fly agaric",
+			Description: "Amanita muscaria. The powder from caps in capsules",
+			Price:       5,
+			Quantity:    10,
+			PackageForm: "60 capsules",
+			Sku:         "EXO-CAP-FAG",
+		},
+	}
+
+	nameQuery := strings.ToLower(filters.Name)
+
+	if nameQuery != "" &&
+		(strings.Contains("amanita muscaria", nameQuery) || strings.Contains("fly agaric", nameQuery)) {
+		commodities = []types.Commodity{
+			{
+				Id:          "266b9823-9b87-4d73-a0f8-41a2b6c5e833",
+				Category:    "exotic",
+				Name:        "Fly agaric",
+				Description: "Amanita muscaria. The edible caps",
+				Price:       1,
+				Quantity:    8,
+				PackageForm: "1 dried piece",
+				Sku:         "EXO-DRY-FAG",
+			},
+			{
+				Id:          "266b9823-9b87-4d73-a0f8-41a2b6c5e834",
+				Category:    "exotic",
+				Name:        "Fly agaric",
+				Description: "Amanita muscaria. The powder from caps in capsules",
+				Price:       5,
+				Quantity:    10,
+				PackageForm: "60 capsules",
+				Sku:         "EXO-CAP-FAG",
+			},
+		}
 	}
 
 	return commodities, nil
